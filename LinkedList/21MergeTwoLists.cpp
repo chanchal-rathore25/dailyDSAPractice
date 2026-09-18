38ecd219d1877d57584b358a3675ea39812014a5
Chanchal Rathore <malicanchal@gmail.com>
2026-09-18 22:03:54 +0530

    Solve 21 Merge Two Sorted List

---
+++ LinkedList/21MergeTwoLists.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    explicit ListNode(int value) : val(value), next(nullptr) {}
+};
+
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL || list2 == NULL){
+            return list1 == NULL ? list2 : list1;
+        }
+
+        if(list1->val<=list2->val){
+            list1->next = mergeTwoLists(list1->next,list2);
+            return list1;
+        }else{
+            list2->next = mergeTwoLists(list1,list2->next);
+            return list2;
+        }
+    }
+};
+
+int main(){
+    Solution sol;
+    ListNode* head1 = new ListNode(3);
+    head1->next = new ListNode(2);
+    head1->next->next = new ListNode(0);
+    head1->next->next->next = new ListNode(-4);
+    head1->next->next->next->next = head1->next; // Creating a cycle
+
+    ListNode* res = sol.mergeTwoLists(head1, nullptr);
+    if(res != NULL){
+        cout << "Reversed list values: ";
+        ListNode* temp = res;
+        while(temp != NULL){
+            cout << temp->val << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+    else{
+        cout << "No nodes in the list." << endl;
+    }
+    
+    return 0;
+
+}
